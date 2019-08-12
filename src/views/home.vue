ccde290b98d3777960a6b59cf27c4416e0805c67
fj <1369431567@qq.com>
2019-08-12 18:22:57 +0800

    init

---
+++ src/views/home.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <div v-if="selected=='tab3'">
+      aa
+    </div>
+    <div v-else>
+      bbb
+    </div>
+    <mt-tabbar v-model="selected">
+      <mt-tab-item id="tab3">
+        <img slot="icon" src="../assets/logo.png" />
+        首页
+      </mt-tab-item>
+      <mt-tab-item id="tab4">
+        <img slot="icon" src="../assets/logo.png" />
+        我的
+      </mt-tab-item>
+    </mt-tabbar>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Home",
+  data(){
+    return{
+      selected:'tab3'
+    }
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
